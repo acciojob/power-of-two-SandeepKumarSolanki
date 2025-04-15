027057daf7d0ce3667b838d04317839e1aa56740
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-04-15 06:51:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	 // Check if number is greater than 0 and if the bitwise AND between number and number-1 is 0
+  return number > 0 && (number & (number - 1)) === 0;
 };
 
 const num = parseInt(prompt("Enter a number"));
